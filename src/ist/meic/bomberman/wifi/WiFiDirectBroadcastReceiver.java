6fc9c55bcac50431beb76314ee62b5ee48ce515d
Antonio Goulao <goulao80.3@gmail.com>
2014-05-13 14:16:48 +0100

    Start implementing WiFi Direct. Create FindServer and Createserver classes

---
+++ src/ist/meic/bomberman/wifi/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,43 @@
+package ist.meic.bomberman.wifi;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.widget.Toast;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private Activity mActivity;
+
+    public WiFiDirectBroadcastReceiver(Activity activity) {
+        super();
+        this.mActivity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+        	
+        	int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Toast.makeText(mActivity, "WiFi Direct Enabled", Toast.LENGTH_SHORT).show();
+            } else {
+            	Toast.makeText(mActivity, "WiFi Direct Disabled", Toast.LENGTH_SHORT).show();
+            }
+        	
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+
+}
