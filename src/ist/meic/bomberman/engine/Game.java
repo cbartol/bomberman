705a8d57f617f73ab0a5a200223d15de0a54fcbb
Alexandra Sankova <sandra.sankova@gmail.com>
2014-05-15 23:07:56 +0100

    Add moveRobot and explosion control

--- src/ist/meic/bomberman/engine/Game.java
+++ src/ist/meic/bomberman/engine/Game.java
@@ -543,6 +543,13 @@ import android.util.SparseArray;
 		return result;
 	}
 	
+	public Map<Integer, Robot> getRobots() {
+		return robots;
+	}
+
+	public void setRobots(Map<Integer, Robot> robots) {
+		this.robots = robots;
+	}
 	protected void destroyObject(char type, int id){
 		// .....
 	}
